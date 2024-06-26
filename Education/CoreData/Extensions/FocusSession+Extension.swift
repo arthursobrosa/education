cb9ceadd46b43c40c4edea51d28aeb13948e6117
Arthur Lopes <arthurpsl99@gmail.com>
2024-06-26 16:59:34 -0300

    CRUD done

---
+++ Education/CoreData/Extensions/FocusSession+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  FocusSession+Extension.swift
+//  Education
+//
+//  Created by Arthur Sobrosa on 26/06/24.
+//
+
+import Foundation
+
+extension FocusSession {
+    var unwrappedID: String {
+        return id ?? String()
+    }
+    
+    var unwrappedTotalTime: Int {
+        return Int(totalTime)
+    }
+}
---
+++ Education/CoreData/Extensions/Theme+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Theme+Extension.swift
+//  Education
+//
+//  Created by Arthur Sobrosa on 26/06/24.
+//
+
+import Foundation
+
+extension Theme {
+    var unwrappedID: String {
+        return id ?? String()
+    }
+    
+    var unwrappedName: String {
+        return name ?? String()
+    }
+}
