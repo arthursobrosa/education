ca191185222471937f462d69df26200723b1f946
Leandro Silva <leandro.nobrega2001@gmail.com>
2024-07-10 17:00:41 -0300

    finish studyTimeView low fidelity

--- Education/StudyTime/StudyTimeCoordinator.swift
+++ Education/StudyTime/StudyTimeCoordinator.swift
@@ -7,6 +7,29 @@
 
 import UIKit
 
-class StudyTimeCoordinator {
+class StudyTimeCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    func start() {
+        self.navigationController.delegate = self
+        self.navigationController.navigationBar.prefersLargeTitles = true
+        
+        let viewModel = StudyTimeViewModel()
+        let vc = StudyTimeViewController(viewModel: viewModel)
+        vc.title = "Study Time"
+        self.navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = self.navigationController.transitionCoordinator?.viewController(forKey: .from) else { return }
+        
+        if self.navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+    }
     
 }
--- Education/StudyTime/StudyTimeViewModel.swift
+++ Education/StudyTime/StudyTimeViewModel.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class StudyTimeViewModel {
+    let subjectmanager: SubjectManager
     
+    init(subjectmanager: SubjectManager = SubjectManager()) {
+        self.subjectmanager = subjectmanager
+    }
+    
+    var subjects = Box([Subject]())
+    
+    func fetchSubjects() {
+        //addSubjects()
+        if let subjects = self.subjectmanager.fetchSubjects() {
+            self.subjects.value = subjects
+        }
+    }
+//    func addSubjects() {
+//        self.subjectmanager.createSubject(name: "Matematica")
+//        self.subjectmanager.createSubject(name: "Portugues")
+//        self.subjectmanager.createSubject(name: "Biologia")
+//        self.subjectmanager.createSubject(name: "historia")
+//        self.subjectmanager.createSubject(name: "Literatura")
+//        self.subjectmanager.createSubject(name: "Artes")
+//    }
+    
+    func removeSubject(subject: Subject) {
+        self.subjectmanager.deleteSubject(subject)
+        self.fetchSubjects()
+    }
 }
