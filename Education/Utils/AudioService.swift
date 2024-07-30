9ef864daafc8be80e8bd9170bdbfbfc776b77225
eppdalencon <eppdalencon@gmail.com>
2024-07-30 16:14:59 -0300

    Adicionando som ao final do timer

---
+++ Education/Utils/AudioService.swift
@@ -0,0 +1,44 @@
+//
+//  AudioService.swift
+//  AudioService
+//
+//  Created by Eduardo Dalencon on 30/07/24.
+//
+
+import Foundation
+import AVFoundation
+var audioPlayer: AVAudioPlayer?
+
+class AudioService {
+    private var timer: Timer?
+    
+    init() {}
+    
+    func playAudio(from url: URL) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.prepareToPlay()
+                audioPlayer?.play()
+                
+            } catch {
+                print("Erro ao tocar áudio: \(error.localizedDescription)")
+            }
+        }
+    
+    func stopAudio() {
+        audioPlayer?.stop()
+    }
+    
+    func pauseAudio() {
+        audioPlayer?.pause()
+    }
+    
+    func resumeAudio() {
+        audioPlayer?.play()
+    }
+    
+    func setVolume(to volume: Float) {
+        audioPlayer?.volume = volume
+    }
+}
+
