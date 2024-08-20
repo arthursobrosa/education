28b3a94d163227ce22cde8c85d96b23319716d1e
LucasMCunha <lucascunha02@hotmail.com>
2024-08-20 15:55:16 -0300

    branch fix

--- Education/ScheduleDetailsModal/ScheduleDetailsModalController+Delegate.swift
+++ Education/ScheduleDetailsModal/ScheduleDetailsModalController+Delegate.swift
@@ -33,16 +33,4 @@ extension ScheduleDetailsModalViewController: ScheduleDetailsModalDelegate {
     func dismiss() {
         self.coordinator?.dismiss(animated: true)
     }
-    
-    func editButtonTapped() {
-        self.coordinator?.showScheduleDetails(schedule: self.viewModel.schedule, selectedDay: self.viewModel.selectedDayIndex)
-    }
-    
-    func startButtonTapped() {
-        let schedule = self.viewModel.schedule
-        
-        let newFocusSessionModel = FocusSessionModel(isPaused: false, subject: self.viewModel.subject, blocksApps: schedule.blocksApps, isAlarmOn: schedule.imediateAlarm, color: self.color)
-        
-        self.coordinator?.showFocusSelection(focusSessionModel: newFocusSessionModel)
-    }
 }
