bbe7b04b6f1dcfb1feb40f550096fc0a904463cc
Arthur Lopes <arthurpsl99@gmail.com>
2024-08-28 18:56:27 -0300

    cocon-regular font added

---
+++ Education/Utils/Fonts/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  Education
+//
+//  Created by Arthur Sobrosa on 28/08/24.
+//
+
+import Foundation
+
+enum Fonts {
+    static let coconRegular = "Cocon-Regular"
+    static let titleSize: CGFloat = 24
+}
