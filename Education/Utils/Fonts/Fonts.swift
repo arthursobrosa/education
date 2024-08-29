d88b493d3ffb20544bda3508abf336871ce17ce2
Arthur Lopes <arthurpsl99@gmail.com>
2024-08-29 16:55:25 -0300

    fonts updated

--- Education/Utils/Fonts/Fonts.swift
+++ Education/Utils/Fonts/Fonts.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 enum Fonts {
-    static let coconRegular = "Cocon-Regular"
+    static let coconRegular = "Cocon-Regular" // for titles
     static let titleSize: CGFloat = 24
+    
+    static let darkModeOnRegular = "DarkmodeOnTrial-Regular"
+    static let darkModeOnMedium = "DarkmodeOnTrial-Medium"
+    static let darkModeOnSemiBold = "DarkmodeOnTrial-SemiBold"
+    static let darkModeOnBold = "DarkmodeOnTrial-Bold"
 }
