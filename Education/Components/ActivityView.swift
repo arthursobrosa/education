0d8e8c36e54c5263f468615c41a69d9556982810
Arthur Lopes <arthurpsl99@gmail.com>
2024-10-18 15:18:10 -0300

    starting progressive player

--- Education/Components/ActivityView.swift
+++ Education/Components/ActivityView.swift
@@ -8,16 +8,18 @@
 import UIKit
 
 class ActivityView: UIView {
+    // MARK: - Delegate to connect with tab bar
     weak var delegate: TabBarDelegate?
     
+    // MARK: - Properties
     var subject: Subject? {
         didSet {
             if let subject {
-                self.activityTitle.text = subject.unwrappedName
+                activityTitle.text = subject.unwrappedName
                 return
             }
             
-            self.activityTitle.text = String(localized: "newActivity")
+            activityTitle.text = String(localized: "newActivity")
         }
     }
     
@@ -25,15 +27,15 @@ class ActivityView: UIView {
         didSet {
             guard let timerSeconds else { return }
             
-            self.updateTimer(timerSeconds: timerSeconds)
+            updateTimer(timerSeconds: timerSeconds)
         }
     }
     
     var color: UIColor? {
         didSet {
-            self.studyingNowLabel.textColor = color?.darker(by: 1.3)
-            self.activityTitle.textColor = color?.darker(by: 0.6)
-            self.activityTimer.textColor = color
+            studyingNowLabel.textColor = color?.darker(by: 1.3)
+            activityTitle.textColor = color?.darker(by: 0.6)
+            activityTimer.textColor = color
         }
     }
     
@@ -42,10 +44,11 @@ class ActivityView: UIView {
             guard let color,
                   let progress else { return }
             
-            self.addLiveActivityButton(color: color, progress: progress)
+            addLiveActivityButton(color: color, progress: progress)
         }
     }
     
+    // MARK: - UI Properties
     private let studyingNowLabel: UILabel = {
         let lbl = UILabel()
         lbl.text = String(localized: "studyingNow")
@@ -74,88 +77,92 @@ class ActivityView: UIView {
         return lbl
     }()
     
+    var liveActivityButton: LiveActivityButton?
+    
+    // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = .systemBackground
-        self.layer.shadowColor = UIColor.label.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: -4)
-        self.layer.shadowOpacity = 0.1
-        self.layer.shadowRadius = 20
+        backgroundColor = .systemBackground
+        layer.shadowColor = UIColor.label.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: -4)
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 20
         
-        self.registerForTraitChanges([UITraitUserInterfaceStyle.self]) {
+        registerForTraitChanges([UITraitUserInterfaceStyle.self]) {
             (self: Self, previousTraitCollection: UITraitCollection) in
             
             self.layer.shadowColor = UIColor.label.cgColor
         }
         
-        self.setupUI()
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Lifecycle
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.layer.cornerRadius = self.bounds.height * (18/60)
+        layer.cornerRadius = bounds.height * (18 / 60)
     }
     
+    // MARK: - Methods
     private func updateTimer(timerSeconds: Int) {
         let seconds = timerSeconds % 60
         let minutes = timerSeconds / 60 % 60
         let hours = timerSeconds / 3600
         
-        let secondsText = self.getText(from: seconds)
-        let minutesText = self.getText(from: minutes)
-        let hoursText = self.getText(from: hours)
+        let secondsText = getText(from: seconds)
+        let minutesText = getText(from: minutes)
+        let hoursText = getText(from: hours)
         
         let timerText = "\(hoursText):\(minutesText):\(secondsText)"
         
-        self.activityTimer.text = timerText
+        activityTimer.text = timerText
     }
     
     private func getText(from number: Int) -> String {
         return number < 10 ? "0\(number)" : "\(number)"
     }
-    
-    @objc private func didTapPlayButton() {
-        self.delegate?.didTapPlayButton()
-    }
 }
 
+// MARK: - UI Setup
 extension ActivityView: ViewCodeProtocol {
     func setupUI() {
-        self.addSubview(studyingNowLabel)
-        self.addSubview(activityTitle)
-        self.addSubview(activityTimer)
+        addSubview(studyingNowLabel)
+        addSubview(activityTitle)
+        addSubview(activityTimer)
         
         NSLayoutConstraint.activate([
-            studyingNowLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 7),
-            studyingNowLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 14),
+            studyingNowLabel.topAnchor.constraint(equalTo: topAnchor, constant: 7),
+            studyingNowLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 14),
             
-            activityTitle.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -11),
+            activityTitle.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -11),
             activityTitle.leadingAnchor.constraint(equalTo: studyingNowLabel.leadingAnchor),
             
-            activityTimer.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: (80/366)),
-            activityTimer.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            activityTimer.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 80 / 366),
+            activityTimer.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
     }
     
     private func addLiveActivityButton(color: UIColor?, progress: CGFloat) {
-        let liveActivityButton = LiveActivityButton(color: color, progress: progress)
-        liveActivityButton.playButton.addTarget(self, action: #selector(didTapPlayButton), for: .touchUpInside)
-        liveActivityButton.translatesAutoresizingMaskIntoConstraints = false
+        liveActivityButton = LiveActivityButton(color: color, progress: progress)
+        liveActivityButton?.pauseResumeButton.addTarget(delegate, action: #selector(TabBarDelegate.didTapPlayButton), for: .touchUpInside)
+        liveActivityButton?.translatesAutoresizingMaskIntoConstraints = false
         
-        self.addSubview(liveActivityButton)
+        guard let liveActivityButton else { return }
+        
+        addSubview(liveActivityButton)
         
         NSLayoutConstraint.activate([
             liveActivityButton.leadingAnchor.constraint(equalTo: activityTimer.trailingAnchor, constant: 13),
-            liveActivityButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: (44/366)),
+            liveActivityButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 44 / 366),
             liveActivityButton.heightAnchor.constraint(equalTo: liveActivityButton.widthAnchor),
-            liveActivityButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            liveActivityButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12)
+            liveActivityButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            liveActivityButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12)
         ])
     }
 }
--- Education/Schedule/FocusPicker/FocusPickerViewModel.swift
+++ Education/Schedule/FocusPicker/FocusPickerViewModel.swift
@@ -37,6 +37,7 @@ class FocusPickerViewModel {
         blockingManager.removeShields()
     }
     
+    #warning("change this afterwards")
     private func setTimerCase() {
         switch focusSessionModel.timerCase {
             case .pomodoro:
