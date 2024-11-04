ceb12a49e94fa24fe40d5846a1e4e90a86f9192c
Leandro Silva <leandro.nobrega2001@gmail.com>
2024-11-04 14:25:30 -0300

    finish UI popover of subjects and week selections

--- Education/Schedule/ScheduleDetails/Extensions/ScheduleDetails+Picker.swift
+++ Education/Schedule/ScheduleDetails/Extensions/ScheduleDetails+Picker.swift
@@ -23,7 +23,7 @@ extension ScheduleDetailsViewController: UIPickerViewDataSource, UIPickerViewDel
         case 0:
             return viewModel.days.count
         case 1:
-            return viewModel.subjectsNames.count + 1
+            return viewModel.subjectsNames.count
         default:
             break
         }
@@ -37,64 +37,96 @@ extension ScheduleDetailsViewController: UIPickerViewDataSource, UIPickerViewDel
         if pickerLabel == nil {
             pickerLabel = UILabel()
             pickerLabel?.font = UIFont(name: Fonts.darkModeOnRegular, size: 17)
-            pickerLabel?.textColor = .systemText30
+            pickerLabel?.textColor = .systemText50
             pickerLabel?.textAlignment = .center
         }
 
         switch pickerView.tag {
         case 0:
             pickerLabel?.text = viewModel.days[row]
+            updateDaySelectionIndicator(pickerView: pickerView, component: component)
         case 1:
-            if row < viewModel.subjectsNames.count {
-                pickerLabel?.text = viewModel.subjectsNames[row]
-            } else {
-                pickerLabel?.text = "Criar Nova"
-                pickerLabel?.font = UIFont(name: Fonts.darkModeOnRegular, size: 17)
-                pickerLabel?.textColor = .bluePicker
-            }
+            pickerView.subviews.last?.backgroundColor = .clear
+            pickerLabel?.text = viewModel.subjectsNames[row]
+            updateSubjectSelectionIndicator(pickerView: pickerView, component: component)
         default:
             break
         }
         
         let selectedRow = pickerView.selectedRow(inComponent: component)
-        if row == selectedRow {
-            if row < viewModel.subjectsNames.count {
+        if pickerView.tag == 1 {
+            if row == selectedRow {
                 let subjectColorName = viewModel.getColorBySubjectName(name: viewModel.selectedSubjectName)
-                pickerLabel?.textColor = UIColor(named: subjectColorName)
-            } else {
-                pickerLabel?.textColor = .bluePicker
+                pickerLabel?.textColor = UIColor(named: subjectColorName)?.darker(by: 0.8)
+            }
+        }else {
+            if row == selectedRow {
+                pickerLabel?.textColor = .systemText50
             }
         }
-
         return pickerLabel ?? UIView()
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerView.subviews.last?.backgroundColor = .clear
         switch pickerView.tag {
         case 0:
             viewModel.selectedDay = viewModel.days[row]
-            updateCellAccessory(for: viewModel.selectedDay, at: pickerView.tag, color: nil)
+            updateCellAccessory(for: viewModel.selectedDay, at: pickerView.tag, color: "system-text-30")
+            updateDaySelectionIndicator(pickerView: pickerView, component: component)
         case 1:
-            if row < viewModel.subjectsNames.count {
-                viewModel.selectedSubjectName = viewModel.subjectsNames[row]
-                updateCellAccessory(for: viewModel.selectedSubjectName, at: pickerView.tag, color: viewModel.getColorBySubjectName(name: viewModel.selectedSubjectName))
-            } else {
-                handleCreateNewSubject()
-            }
+            viewModel.selectedSubjectName = viewModel.subjectsNames[row]
+            updateCellAccessory(for: viewModel.selectedSubjectName, at: pickerView.tag, color: viewModel.getColorBySubjectName(name: viewModel.selectedSubjectName))
+            updateSubjectSelectionIndicator(pickerView: pickerView, component: component)
+
         default:
             break
         }
-        
         pickerView.reloadComponent(component)
+        
+    }
+    
+    private func updateSubjectSelectionIndicator(pickerView: UIPickerView, component: Int) {
+        pickerView.subviews.filter { $0.tag == 99 }.forEach { $0.removeFromSuperview() }
+        pickerView.subviews.last?.backgroundColor = .clear
+
+        let subjectColorName = viewModel.getColorBySubjectName(name: viewModel.selectedSubjectName)
+        guard let baseColor = UIColor(named: subjectColorName) else { return }
+        let color = baseColor.withAlphaComponent(0.2)
+        
+        let rowHeight = pickerView.rowSize(forComponent: component).height
+        let selectionIndicatorY = (pickerView.bounds.height - rowHeight) / 2
+        let frame = CGRect(x: 0, y: selectionIndicatorY, width: pickerView.bounds.width, height: rowHeight)
+        
+        let overlayView = UIView(frame: frame)
+        overlayView.backgroundColor = color
+        overlayView.roundCorners(corners: .allCorners, radius: 10, borderWidth: 0, borderColor: .clear)
+        overlayView.isUserInteractionEnabled = false
+        overlayView.tag = 99
+        
+        pickerView.insertSubview(overlayView, at: 0)
+    }
+    
+    private func updateDaySelectionIndicator(pickerView: UIPickerView, component: Int) {
+        pickerView.subviews.filter { $0.tag == 99 }.forEach { $0.removeFromSuperview() }
+        pickerView.subviews.last?.backgroundColor = .clear
+        
+        let rowHeight = pickerView.rowSize(forComponent: component).height
+        let selectionIndicatorY = (pickerView.bounds.height - rowHeight) / 2
+        let frame = CGRect(x: 0, y: selectionIndicatorY, width: pickerView.bounds.width, height: rowHeight)
+        
+        let overlayView = UIView(frame: frame)
+        overlayView.backgroundColor = .TOGGLE_BG
+        overlayView.roundCorners(corners: .allCorners, radius: 10, borderWidth: 0, borderColor: .clear)
+        overlayView.isUserInteractionEnabled = false
+        overlayView.tag = 99
+        
+        pickerView.insertSubview(overlayView, at: 0)
     }
     
     private func updateCellAccessory(for text: String, at section: Int, color: String?) {
         let cell = scheduleDetailsView.tableView.cellForRow(at: IndexPath(row: 0, section: section))
         let label = createLabel(text: text, color: color)
         cell?.accessoryView = label
-    }
-    
-    @objc func handleCreateNewSubject() {
-        print("Criar Nova opção selecionada")
     }
 }
